6248d69208c4f280122078e94831af4db3a26af9
root <tom.l.athey@gmail.com>
2018-08-21 18:44:08 +0000

    pathology

--- pathology_simple.py
+++ pathology_simple.py
@@ -9,10 +9,12 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Activation
 from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
 from keras import backend as K
+from keras.utils import multi_gpu_model
+
 import numpy as np
-from sklearn.model_selection import GridSearchCV
+
 from keras.wrappers.scikit_learn import KerasClassifier
-from keras.optimizers import Adadelta
+from keras.optimizers import Adadelta, SGD
 
 
 num_classes = 4
@@ -46,40 +48,55 @@ print(x_test.shape[0], 'test samples')
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-def create_model(dropout_rate=0.0, fc_neurons=256, num_layers=5, learn_rate=0.01, decay=0):
-	model = Sequential()
-	for i in range(num_layers):
-		num_filters = 2**(i+4)
-		if i==0:
-			model.add(Conv2D(num_filters, kernel_size=(3,3), input_shape=input_shape))
-		else:
-			model.add(Conv2D(num_filters, kernel_size=(3,3)))
-		model.add(BatchNormalization(axis=3))
-		model.add(Activation('relu'))
-		model.add(MaxPooling2D(2,2))
+def create_model(dropout_rate, fc_neurons, num_layers, lr):
+        model = Sequential()
+        #model.add(BatchNormalization(axis=3,input_shape=input_shape))
+        for i in range(num_layers):
+                num_filters = 2**(i+4)
+                if i==0:
+                        model.add(Conv2D(num_filters, kernel_size=(3,3),
+                                input_shape=input_shape))
+                else:
+                        model.add(Conv2D(num_filters, kernel_size=(3,3)))
+                model.add(BatchNormalization(axis=3))
+                model.add(Activation('relu'))
+                model.add(MaxPooling2D(2,2))
 
-	model.add(Flatten())
-	model.add(Dense(fc_neurons, activation='relu'))
-	model.add(Dropout(dropout_rate))
-	model.add(Dense(num_classes, activation='softmax'))
-	
-	optimizer = Adadelta(lr=learn_rate, decay=decay)
+        model.add(Flatten())
+        model.add(Dense(fc_neurons, activation='relu'))
+        model.add(Dropout(dropout_rate))
+        model.add(Dense(num_classes, activation='softmax'))
+
+        optimizer = SGD(lr=10,decay=0.0,momentum=0.9, nesterov=True)
+
         parallel_model = multi_gpu_model(model,gpus=4)
-	parallel_model.compile(loss=keras.losses.categorical_crossentropy,
-		optimizer='Adadelta', metrics = ['accuracy'])
-	return parallel_model
+        parallel_model.compile(loss=keras.losses.categorical_crossentropy,
+                optimizer=optimizer, metrics = ['accuracy'])
+        return parallel_model
 
 
-batch_size = [2,4]
-epochs = [30,60]
-learn_rate = [0.1, 0.001]
-decay = [0.0, 0.1]
-dropout_rate = [0.2, 0.5]
-fc_neurons = [128, 256]
-num_layers = [5, 8]
+#new
+batch_size = [12]
+epochs = [60]
+dropout_rate = [0.5]
+fc_neurons = [128]
+num_layers = [5]
+learn_rate = [0.01]
 
-total_models = len(batch_size)*len(epochs)*len(learn_rate)*len(decay)*len(dropout_rate)*len(fc_neurons)*len(num_layers)
+total_models = len(batch_size)*len(epochs)*len(dropout_rate)*len(fc_neurons)*len(num_layers)*len(learn_rate)
 counter = 0
+
+model = create_model(dropout_rate=dropout_rate[0], fc_neurons=fc_neurons[0],
+                     num_layers=num_layers[0], lr=learn_rate[0])
+his = model.fit(x=x_train,y=y_train,
+                batch_size=batch_size[0], epochs=epochs[0], verbose=1,
+                validation_data=(x_test,y_test))
+print('Train accuracy****************************************')
+print(his.history['acc'])
+print('Validation accuracy************************************')
+print(his.history['val_acc'])
+#
+'''
 
 with open('/workspace/results_keras/gridsearch.txt','w') as f:
   f.write('[Loss, accuracy]')
@@ -98,3 +115,4 @@ with open('/workspace/results_keras/gridsearch.txt','w') as f:
     print(txt)
     f.write(str(txt))
     f.write('\n')
+'''
